b8f8693ab68bf6289c6ec82da22a1a4ed7f44f7f
Daalmodi <daalmodi@gmail.com>
2024-10-03 06:02:16 -0500

    Adjust cards

--- Frontend/English-Skills-Tracker/src/app/home-page/home-page.component.ts
+++ Frontend/English-Skills-Tracker/src/app/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { CardTemplateComponent } from '../card-template/card-template.component';
+import { FooterComponent } from '../footer/footer.component';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [HeaderComponent,CardTemplateComponent],
+  imports: [HeaderComponent,CardTemplateComponent,FooterComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.sass'
 })
