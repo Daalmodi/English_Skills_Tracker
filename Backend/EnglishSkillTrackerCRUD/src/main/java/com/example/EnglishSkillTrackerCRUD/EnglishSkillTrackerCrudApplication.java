af2d8945e1e3c234d3e1ba7865f9611bb67ac20b
Daniel Moya <daalmodi@gmail.com>
2024-11-11 11:42:12 -0500

    Spring boot add .

---
+++ Backend/EnglishSkillTrackerCRUD/src/main/java/com/example/EnglishSkillTrackerCRUD/EnglishSkillTrackerCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.EnglishSkillTrackerCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishSkillTrackerCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishSkillTrackerCrudApplication.class, args);
+	}
+
+}
