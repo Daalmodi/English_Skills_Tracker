0f9e07fd6bee0313fb60189bb21e63544ea79abc
Daniel Moya <daalmodi@gmail.com>
2024-11-30 17:40:57 -0500

    Autenticacion iniciada falta completar

--- Backend/EnglishSkillTrackerCRUD/src/main/java/com/example/EnglishSkillTrackerCRUD/controller/UserController.java
+++ Backend/EnglishSkillTrackerCRUD/src/main/java/com/example/EnglishSkillTrackerCRUD/controller/UserController.java
@@ -3,7 +3,10 @@ package com.example.EnglishSkillTrackerCRUD.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties.Authentication;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -46,6 +49,29 @@ public class UserController {
         
         return ResponseEntity.ok(userDTOEdited);
     }
+
+    // Metodos de Autenticacion y autorizacion 
+    @PostMapping("/loggin")
+    public String login(@RequestBody UserDTO userDTO) {
+        UsernamePasswordAuthenticationToken authenticationToken =new UsernamePasswordAuthenticationToken(userDTO.getName(), userDTO.getPassword());
+      
+        return "Token de autenticacion";
+    }
+
+    @PostMapping("/register")
+    public String postMethodName(@RequestBody UserDTO userDTO) {
+        
+        
+        return "userDTO";
+    }
+    @GetMapping("/profile")
+    public UserDTO getProfile(@AuthenticationPrincipal UserDTO userDTO) {
+        // Lógica para obtener el perfil del usuario
+        // ...
+        return userDTO;
+    }
+    
+    
 
 
 }
