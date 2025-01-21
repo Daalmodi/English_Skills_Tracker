a6a5eec33ad400a5535af6578b3a8e982baa925e
Daniel Moya <daalmodi@gmail.com>
2025-01-21 18:31:51 -0500

    Login LOGOUT

--- Frontend/English-Skills-Tracker/src/app/components/header/header.component.ts
+++ Frontend/English-Skills-Tracker/src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import { MatDialogModule,MatDialog } from '@angular/material/dialog';
@@ -10,7 +10,28 @@ import { LoginDialogComponent } from '../login-dialog/login-dialog.component';
   templateUrl: './header.component.html',
   styleUrl: './header.component.sass'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+session: boolean = false;
+ngOnInit(): void {
+  this.loginState();
+
+}
+
+loginState(){
+  if(typeof localStorage !== 'undefined'){
+    
+    const token = localStorage.getItem('token');
+    if(token){
+      this.session = true;
+    }
+  }else{
+    this.session = false; 
+  }
+}
+
+
+
+
   readonly dialog = inject(MatDialog);
 
 loginForm(){
@@ -18,7 +39,14 @@ loginForm(){
   const dialogRef = this.dialog.open(LoginDialogComponent);
 
   dialogRef.afterClosed().subscribe(()=>{
-    
+    this.loginState();
+    this.session = true;
   });
+
+}
+logout(){
+  localStorage.removeItem('token');
+  this.session = false;
+  this.loginState();
 }
 }
